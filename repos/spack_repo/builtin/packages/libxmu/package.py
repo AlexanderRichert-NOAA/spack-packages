951440d3956eadd5903ed393c33cdebd68d0bf1c
Alex Richert <alexander.richert@noaa.gov>
2026-09-22 14:35:42 -0700

    libxmu: Add dependencies for xproto and kbproto

--- repos/spack_repo/builtin/packages/libxmu/package.py
+++ repos/spack_repo/builtin/packages/libxmu/package.py
@@ -32,6 +32,8 @@ class Libxmu(AutotoolsPackage, XorgPackage):
 
     depends_on("libxt")
     depends_on("libxt@1.1:", when="@1.2:")
+    depends_on("xproto")
+    depends_on("kbproto")
     depends_on("libxext")
     depends_on("libx11")
 
